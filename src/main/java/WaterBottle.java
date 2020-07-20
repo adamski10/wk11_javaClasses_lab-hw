57779245b40ed223292416e3d61e3944d6c1816f
AdamK <44851323+adamski10@users.noreply.github.com>
2020-07-20 16:39:44 +0200

    water bottle done

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,23 @@
 public class WaterBottle {
+    private int volume;
+
+    public WaterBottle(int volume) {
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int drink() {
+        return this.volume - 10;
+    }
+
+    public void empty() {
+        this.volume = 0;
+    }
+
+    public int fill() {
+        return this.volume = 100;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    private WaterBottle bottle;
+
+    @Before
+    public void before() {
+        this.bottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void canDrink() {
+
+        assertEquals(90, bottle.drink());
+    }
+
+    @Test
+    public void canEmptyTheBottle() {
+        bottle.empty();
+        assertEquals(0, bottle.getVolume());
+    }
+
+    @Test
+    public void canFillTheBottle() {
+        bottle.empty();
+        bottle.fill();
+        assertEquals(100, bottle.getVolume());
+    }
 }
